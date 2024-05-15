3eb58e865d781dc6710933dbe901c555a2a3e7bf
Vinícius Marcili <vinipetter@gmail.com>
2024-05-15 17:51:18 -0300

    Finish

--- script.js
+++ script.js
@@ -19,7 +19,11 @@ function calculate(e) {
   const resultDialog = document.getElementById('result')
   bmiResultElement.innerHTML = `${bmi.toFixed(2)}kg/m²`
   json.innerHTML = `${JSON.stringify(bmiResult, null, 2)}`
+  history.push(bmiResult)
+  renderHistory()
   resultDialog.showModal()
+
+  form.reset()
 }
 
 function reset() {
@@ -33,4 +37,15 @@ function reset() {
 function closeResult() {
   const resultDialog = document.getElementById('result')
   resultDialog.close()
+}
+
+function renderHistory() {
+  const historyElement = document.getElementById('history')
+  historyElement.innerHTML = ''
+  history.forEach((item, index) => {
+    const listItem = document.createElement('li')
+    listItem.innerHTML = `<h6>#${index + 1}: ${item.bmi.toFixed(2)}kg/m²</h6>`
+    listItem.innerHTML += `<pre>${JSON.stringify(item, null, 2)}</pre>`
+    historyElement.appendChild(listItem)
+  })
 }
